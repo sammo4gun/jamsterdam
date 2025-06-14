09c399d107c15c53e02bb22fdb4cdbed76043a3a
Alin Ali Hassan <hassanalinali@gmail.com>
2025-06-14 16:45:12 +0200

    Fixed collision for roses

--- Scripts/rose.gd
+++ Scripts/rose.gd
@@ -6,6 +6,6 @@ func _ready():
 	self.body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Guy"):
 		print("Player picked up the rose!")
 		get_tree().change_scene_to_file(next_scene_path)
