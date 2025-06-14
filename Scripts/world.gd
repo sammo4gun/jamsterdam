68b1ecc013b00329976fa872d2c16ef17f4a4de1
sammo4gun <33280495+sammo4gun@users.noreply.github.com>
2025-06-14 13:43:05 +0200

    ghost updates

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -4,9 +4,12 @@ extends Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	## Mouse in viewport coordinates.
-	#if event is InputEventMouseButton and event.pressed:
-		#Ghost.set_target_pos(event.global_position)
 	pass
 
 func attempt_possession(body: CharacterBody2D):
 	Ghost.set_target_possess(body)
+
+func _on_background_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		Ghost.set_target_pos(event.global_position)
+	pass # Replace with function body.
