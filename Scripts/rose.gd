b6103cd6c1a16b55320ee8206054464150902889
Alin Ali Hassan <hassanalinali@gmail.com>
2025-06-14 17:00:58 +0200

    Added rose dynamic level advancement

--- Scripts/rose.gd
+++ Scripts/rose.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
-@export var next_scene_path: String = "res://Scenes/Levels/level2.tscn"
-
 func _ready():
+	print(get_tree().current_scene.name)
 	self.body_entered.connect(_on_body_entered)
+
+func next_level():
+	var current_name = get_tree().current_scene.name
+
+	# Use regex to extract digits from the name
+	var regex = RegEx.new()
+	regex.compile(r"\d+")
+	var result = regex.search(current_name)
+
+	if result:
+		var number = int(result.get_string())
+		var next_number = number + 1
+		var next_scene_path = "res://Scenes/Levels/level%d.tscn" % next_number
+		get_tree().change_scene_to_file(next_scene_path)
+	else:
+		print("No number found in scene name")
 
 func _on_body_entered(body):
 	if body.is_in_group("Guy"):
 		print("Player picked up the rose!")
-		get_tree().change_scene_to_file(next_scene_path)
+		next_level()
