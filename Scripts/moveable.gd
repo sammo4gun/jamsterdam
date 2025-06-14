19c26bab0eec2efda4ca19f26dc0c9d25d72f41e
sammo4gun <33280495+sammo4gun@users.noreply.github.com>
2025-06-14 17:53:05 +0200

    added shader

--- Scripts/moveable.gd
+++ Scripts/moveable.gd
@@ -4,6 +4,7 @@ class_name Moveable extends CharacterBody2D
 @onready var shape = $"Shape"
 @onready var guy = null
 @onready var guyfinder = $"GuyFinder"
+@onready var aura = $"Whitecircle"
 
 @export var speed = 2000
 @export var baseweight = 8
@@ -13,8 +14,12 @@ var possessed = false
 var weight = 0
 var connected_guy = false
 
+var corr_aura_scale
+
 func _ready() -> void:
 	weight = baseweight + (scale.x * scale.y) * changeweight
+	corr_aura_scale = aura.scale
+	aura.scale *= 0.01
 
 func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -24,10 +29,14 @@ func get_input(delta):
 	velocity = velocity.move_toward(target_velocity, 1000 * delta)
 	if connected_guy and velocity.y < 0:
 		guy.get_parent().velocity.y = velocity.y
-	
 
 func _physics_process(delta):
 	if possessed:
+		print(aura.scale)
+		if aura.scale.x < corr_aura_scale.x/2:
+			aura.scale = corr_aura_scale/2
+		if aura.scale.x < corr_aura_scale.x:
+			aura.scale *= 1.1
 		if len(guyfinder.get_overlapping_areas()) == 0:
 			connected_guy = false
 			guy = null
@@ -40,6 +49,9 @@ func _physics_process(delta):
 			guy = null
 		get_input(delta)
 		move_and_slide()
+	else:
+		if aura.scale.x > 0.01:
+			aura.scale *= 0.97
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	get_viewport().set_input_as_handled()
