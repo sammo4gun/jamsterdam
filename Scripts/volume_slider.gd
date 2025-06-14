aa86b5796b56069104c27f553465544004998034
Alin Ali Hassan <hassanalinali@gmail.com>
2025-06-14 19:17:07 +0200

    Fixed merge conflicts and save state of volume sliders

--- Scripts/volume_slider.gd
+++ Scripts/volume_slider.gd
@@ -2,11 +2,12 @@ extends HSlider
 
 @export
 var bus_name: String
-
 var bus_index: int
 
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
+	var db_volume = AudioServer.get_bus_volume_db(bus_index)
+	value = db_to_linear(db_volume)
 	value_changed.connect(_on_value_changed)
 
 func _on_value_changed(volume_value: float) -> void:
